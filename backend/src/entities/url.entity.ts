50a1956c22bb606209f586693b01b504d8db1d97
happymalyo <rtafitamario@gmail.com>
2025-07-05 16:32:11 +0300

    chore: typeorm installtion

---
+++ backend/src/entities/url.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Url {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text', nullable: false })
+  originalUrl: string;
+
+  @Column({ type: 'varchar', length: 10, unique: true })
+  shortCode: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ type: 'int', default: 0 })
+  visitCount: number;
+}
